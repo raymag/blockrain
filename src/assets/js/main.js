c762eaf9d6fb68609f4300753142335a8385ed07
raymag <prayk11@outlook.com>
2019-10-08 20:35:15 -0300

    initial state

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,91 @@
 const socket = io()
 
+let game = {
+    canvas,
+    players:undefined,
+    coins:undefined,
+    createGame: (data) => {
+        game.canvas = document.querySelector('#canvas')
+        game.canvas.width = 600
+        game.canvas.height = 600
+        game.ctx = game.canvas.getContext('2d')
+        game.update(data)
+        window.addEventListener('keyup', (e) => {
+            if(e.keyCode == 37){
+                socket.emit('move', {move:'left'})
+            }
+            else if(e.keyCode == 39){
+                socket.emit('move', {move:'right'})
+            }
+        })
+        setInterval(game.render.render, 1000/60)
+    },
+    update: (data) => {
+        game.players = data.players
+        game.coins = data.coins
+    },
+    render: {
+        render: () => {
+            let render = game.render
+            render.clearCanvas()
+            render.renderPlayers()
+            render.renderCoins()
+            requestAnimationFrame(render.render)
+        },
+        renderCoins: () => {
+            for(let i;i<game.coins.length;i++){
+                console.log('x')
+                let ctx = game.ctx
+                ctx.beginPath()
+                ctx.rect(game.coins[i].x, game.coins[i].y, 60, 60)
+                ctx.fillStyle = "#fff"
+                ctx.fill()
+            }
+        },
+        renderPlayer: (player, isLocalPlayer) => {
+            let ctx = game.ctx
+            // let img = new Image()
+            // img.onload = () => {
+            //     ctx.drawImage(img, 10, 10)
+            // }
+            // if(isLocalPlayer){
+            //     img.src = "static/img/player.png"
+            // }else{
+            //     img.src = "static/img/player_red.svg"
+            // }
+            // var img = new Image();
+            // img.onload = function () {
+            //     ctx.drawImage(img, player.x, player.y, 60, 60);
+            // }
+            // img.src = "static/img/player.svg";
+            ctx.beginPath()
+            ctx.rect(player.x, player.y, 60, 60)
+            ctx.fillStyle = "#000"
+            ctx.fill()
+        },
+        renderPlayers: () => {
+            for(let i=0;i<game.players.length;i++){
+                let isLocalPlayer = false
+                if( game.players[i].id == socket.id ){
+                    isLocalPlayer = true
+                }
+                game.render.renderPlayer(game.players[i], isLocalPlayer)
+            }
+        },
+        clearCanvas: () => {
+            let ctx = game.ctx
+            ctx.beginPath()
+            ctx.rect(0, 0, game.canvas.width, game.canvas.height)
+            ctx.fillStyle = '#5555ff'
+            ctx.fill()
+        }
+    }
+}
+
 socket.on('start', (data) => {
-    console.log(`The received message is: ${data.message}`)
+    game.createGame(data)
+})
+
+socket.on('update', (data) => {
+    game.update(data)
 })
--- src/server.js
+++ src/server.js
@@ -9,8 +9,67 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname+'/views/index.html')
 })
 
+const canvasWidth = 600
+const canvasHeight = 600
+const size = 60
+
+let players = []
+let coins = []
+
+function createPlayer(socket){
+    let player = {
+        id: socket.id,
+        x: Math.floor(Math.random()*10)*size,
+        y: canvasHeight-size,
+        nickname: 'thielf',
+        score: 0
+    }
+    players.push(player)
+}
+function update(){
+    io.emit('update', {players, coins})
+}
+function downCoins(){
+    for(let i=0;i<coins.length;i++){
+        coins[i].y += 10
+    }
+    update()
+}
+function createCoin(){
+    let coin = {
+        x: Math.floor(Math.random()*10)*size,
+        y: -100
+    }
+    coins.push(coin)
+    setInterval(downCoins, 3000)
+}
+
 io.on('connection', (socket) => {
-    io.emit('start', {message: '. -.-. .... .. .-.. . -.--     .. ...     -... . .- ..- - .. ..-. ..- .-..'})
+    createPlayer(socket)
+    socket.emit('start', {players, coins})
+    socket.on('disconnect', () => {
+        for(let i=0;i<players.length;i++){
+            if(players[i].id == socket.id){
+                players.splice(i, 1)
+                update()
+            }
+        }
+    })
+    socket.on('move', (data) => {
+        for(let i=0;i<players.length;i++){
+            if(players[i].id == socket.id){
+                createCoin()
+                if(data.move == 'left'){
+                    players[i].x -= 60
+                }
+                else if(data.move == 'right'){
+                    players[i].x += 60
+                }
+                update()
+                break
+            }
+        }
+    })
 })
 
 const PORT = process.env.PORT || 3000
